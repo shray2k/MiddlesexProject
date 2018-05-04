b9214fcbb7b25be81a21937cc85007ca9d496bf8
eomuratoglu <33694486+eomuratoglu@users.noreply.github.com>
2018-05-04 08:35:02 -0400

    person class, empty admin class

---
+++ src/Person.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ * @author ezramuratoglu
+ *
+ */
+public class Person 
+{
+	private String name;
+	private String password;
+	
+	public Person(String name, String password) {
+		this.name = name;
+		this.password = password;
+	}
+	
+	public String getName()
+	{
+		return name;
+	}
+	
+	public String getPassword()
+	{
+		return password;
+	}
+}
+
+
